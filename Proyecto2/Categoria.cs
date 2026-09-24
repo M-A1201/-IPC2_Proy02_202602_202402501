8f9741f55e4e96b7cba4162babba612e0804127b
M-A1201 <2946755580408@ingenieria.usac.edu.gt>
2026-09-23 20:51:53 -0600

    imprementada la estructura de clases base y TDAs iniciales

--- Proyecto2/Categoria.cs
+++ Proyecto2/Categoria.cs
@@ -1,18 +1,63 @@
 using System;
 class Categoria
 {
-    private string categorias;
+    //atributos de la clase
+    private string nombre;
+    private Categoria categoriaPadre;
 
+    private NodoSubCategoria primeraSubCategoria;
+    //como se le llama a esto?
+    private NodoLibro primerLibro;
 
     //metodo 
-    public void MostrarCategoriasAlfabeticamente()
+    public Categoria(string nombre)
     {
-        
+        this.nombre=nombre;
+        this.categoriaPadre=null;
+        this.primeraSubCategoria=null;
+        this.primerLibro=null;
+
     }
 
-    public void MostrarSubCategoriasAlfabeticamente()
+//campos base para la estructura jerarquica
+    public string GetNombre()
     {
-        
+        return nombre;
+    }
+
+    public NodoLibro GetPrimerLibro()
+    {
+        return primerLibro;
+    }
+    public void SetPrimerLibro(NodoLibro nodo)
+    {
+        this.primerLibro=nodo;
+    }
+
+    class NodoSubCategoria
+    {
+        private Categoria categoria;
+        private NodoSubCategoria siguiente;
+        public NodoSubCategoria(Categoria categoria)
+        {
+            this.categoria=categoria;
+            this.siguiente=null;
+        }
+
+        public Categoria GetCategoria()
+        {
+            return categoria;
+        }
+
+        public NodoSubCategoria GetSiguiente()
+        {
+            return siguiente;
+        }
+
+        public void SetSiguiente(NodoSubCategoria siguiente)
+        {
+            this.siguiente=siguiente;
+        }
     }
 
 
--- Proyecto2/obj/Debug/net10.0/Proyecto2.AssemblyInfo.cs
+++ Proyecto2/obj/Debug/net10.0/Proyecto2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f316744a1d9430574c85575b59edddfbf12d21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada9b2046a3279d1cbe39bc7c6584dbbe9fd365f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
